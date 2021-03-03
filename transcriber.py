afaaa731e6c2dcb8a59e1f2e85e4a51281c5b0af
AI-Spawn <50590465+AI-Spawn@users.noreply.github.com>
2021-03-03 12:55:57 -0500

    started language support

--- transcriber.py
+++ transcriber.py
@@ -1,14 +1,28 @@
 import speech_recognition as sr
+import requests
+import json
 
 
-def transcribe(file_name):
+def transcribe(file_name, language="en-US"):
     r = sr.Recognizer()
     with sr.AudioFile(file_name) as source:
         audio = r.record(source)  # read the entire audio file
+        return r.recognize_google(audio, language=language)
 
-        return r.recognize_google(audio)
 
-def create_script(file_name):
-        script = open('generated_script.txt', 'w+')
-        text = transcribe(file_name)
-        script.write(text)
+def create_script(file_name, language='en-US'):
+    script = open('generated_script.txt', 'w+')
+    text = transcribe(file_name, language)
+    script.write(text)
+
+
+def watson(audio, auth):
+    with open(audio, 'rb') as f:
+        data = f.read()
+    header = {"Content-Type": "audio/wav"}
+    r = requests.post(f"{auth['url']}/v1/recognize", data=data, headers=header, auth=('apikey', auth['apikey']))
+    return r.text
+
+
+if __name__ == "__main__":
+    create_script('custom/spn2.wav', language='es-ES')
